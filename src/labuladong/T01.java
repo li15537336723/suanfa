319150bf4526679a0cc0af2f9a970f5791622067
lixianghong <669316349@qq.com>
2022-01-07 21:13:31 +0800

    第一次

---
+++ src/labuladong/T01.java
@@ -0,0 +1,13 @@
+package labuladong;
+
+/**
+ * description:
+ *
+ * @author: lixianghong
+ * @date: 2022/1/7 21:07
+ */
+public class T01 {
+  public static void main(String[] args) {
+    System.out.println("hello world!");
+  }
+}
